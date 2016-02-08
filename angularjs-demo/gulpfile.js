d565ba8c70e0606abb93757571c4bd921eef4711
James Churchill <james@smashdev.com>
2016-02-07 16:39:30 -0800

    Further work on the gulp build

--- angularjs-demo/gulpfile.js
+++ angularjs-demo/gulpfile.js
@@ -1,35 +1,73 @@
 
-var gulp = require('gulp');
-var clean = require('gulp-clean');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    connect = require('gulp-connect'),
+    eslint = require('gulp-eslint');
+
+var bases = {
+    app: 'app/',
+    dist: 'dist/',
+    nodemodules: 'node_modules/'
+};
 
 var paths = {
     scripts: [
-        'node_modules/angular/angular.js',
-        'app/scripts/**/*.js'
+        'scripts/**/*.js'
     ],
-    html: ['app/index.html'],
-    dist: 'dist'
+    libs: [
+        'angular/angular.min.js',
+    ],
+    html: [
+        'index.html'
+    ]
 };
 
 gulp.task('clean', function () {
-    return gulp.src(paths.dist + '/**/*', {read: false})
+    return gulp.src(bases.dist + '**/*', {read: false})
     	.pipe(clean());
 });
 
-gulp.task('concat-js', function () {
-    return gulp.src(paths.scripts)
+gulp.task('lint', function () {
+    return gulp.src(paths.scripts, {cwd: bases.app})
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('scripts', ['clean', 'lint'], function () {
+    return gulp.src(paths.scripts, {cwd: bases.app})
         .pipe(sourcemaps.init())
-        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.dist));
+        .pipe(gulp.dest(bases.dist))
+        .pipe(connect.reload());
 });
 
-gulp.task('copy-html', function () {
-    return gulp.src(paths.html)
-        .pipe(gulp.dest(paths.dist));
+gulp.task('copy-html', ['clean'], function () {
+    return gulp.src(paths.html, {cwd: bases.app})
+        .pipe(gulp.dest(bases.dist));
 });
 
-gulp.task('default', ['clean', 'concat-js', 'copy-html'], function () {
+gulp.task('copy-lib', ['clean'], function () {
+    return gulp.src(paths.libs, {cwd: bases.nodemodules})
+        .pipe(gulp.dest(bases.dist));
 });
+
+gulp.task('build', ['scripts', 'copy-html', 'copy-lib']);
+
+gulp.task('watch', function () {
+    gulp.watch(paths.html.concat(paths.scripts), {cwd: bases.app}, ['build']);
+});
+
+gulp.task('webserver', ['build', 'watch'], function () {
+    connect.server({
+        root: bases.dist,
+        livereload: true
+    });
+});
+
+gulp.task('default', ['build']);
